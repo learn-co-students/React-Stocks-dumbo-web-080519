58d28a598b3cc660c7bc10c90e53d4929b4fb575
Renée Cruz <reneebasscruz@gmail.com>
2019-11-03 21:13:06 -0500

    only sort button is not active yet

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,23 +1,24 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" name="sort" checked={null} />
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" name="sort" checked={null} />
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={props.handleFilterButton} name="filter">
+          <option value="All">All</option>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -4,12 +4,19 @@ import Stock from '../components/Stock'
 class StockContainer extends Component {
 
   render() {
+    // console.log(this.props.stocks)
+
     return (
       <div>
         <h2>Stocks</h2>
         {
-          //render the list of stocks here
+          // render the list of stocks here, do map here(always return the value), send props to Stock Component
+
+          this.props.stocks.map(stock => {
+            return <Stock stock={stock} key={stock.id} handleClick={this.props.handleClick} />
+          })
         }
+
       </div>
     );
   }
